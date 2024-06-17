07a892c5b03c5664a15e1fe0bf7c897afb908f25
drdonice <doniceubaru1@gmail.com>
2024-06-17 23:05:00 +0100

    Push

--- src/components/modules/dashboard/index.tsx
+++ src/components/modules/dashboard/index.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useEffect, useReducer } from "react";
+import React, { useEffect } from "react";
 import WalletCard from "./WalletCard";
 import StatsCard from "./statsCard";
 import { PrimaryButton, SecondaryButton } from "@/src/components/common/button";
@@ -9,14 +9,28 @@ import { fetchDashboardData } from "@/src/services/dashboardService";
 import toast from "react-hot-toast";
 import LoaderSkeleton from "../../common/loader-skeleton";
 
-const initialState = {
-  fidelityData: {
+const DashbaordComponent = () => {
+  const fidelityData: {
+    current_earnings: string | null;
+    wallet_balance: string | null;
+    wallet_id: string | null;
+    wallet_name: string | null;
+  } = {
     current_earnings: null,
     wallet_balance: null,
     wallet_id: null,
     wallet_name: null,
-  },
-  accessData: {
+  };
+
+  const accessData: {
+    account_name: string | null;
+    account_number: string | null;
+    balance: string | null;
+    bank_name: string | null;
+    earnings: string | null;
+    total_credit: string | null;
+    total_debit: string | null;
+  } = {
     account_name: null,
     account_number: null,
     balance: null,
@@ -24,45 +38,14 @@ const initialState = {
     earnings: null,
     total_credit: null,
     total_debit: null,
-  },
-  loading: true,
-};
-
-const reducer = (state: any, action: any) => {
-  switch (action.type) {
-    case "FETCH_SUCCESS":
-      return {
-        ...state,
-        fidelityData: action.payload.fidelityData,
-        accessData: action.payload.accessData,
-        loading: false,
-      };
-    case "FETCH_ERROR":
-      return {
-        ...state,
-        loading: false,
-      };
-    default:
-      return state;
-  }
-};
-
-const DashboardComponent = () => {
-  const [state, dispatch] = useReducer(reducer, initialState);
+  };
 
   const getDashboardData = async () => {
     try {
       const res = await fetchDashboardData();
-      dispatch({
-        type: "FETCH_SUCCESS",
-        payload: {
-          fidelityData: res.fidelityData,
-          accessData: res.accessData,
-        },
-      });
+      console.log(res, "Response");
     } catch (error) {
       toast.error("Error fetching dashboard data");
-      dispatch({ type: "FETCH_ERROR" });
     }
   };
 
@@ -70,25 +53,19 @@ const DashboardComponent = () => {
     getDashboardData();
   }, []);
 
-  const { fidelityData, accessData, loading } = state;
-
   return (
     <div className="dashboard">
       <header className="dashboard_header">
         <CustomHeader title={`Welcome back`} desc="Overview of Dashboard" />
-       
+
+        {accessData && fidelityData ? (
           <div className="dashboard_header_buttons">
             <SecondaryButton
               text="Akara Ekwenti"
               link="/tickets/transport/add"
             />
             <PrimaryButton text="Smart Tickets" link="/tickets/transport/add" />
-          </div> 
-          
-          {!loading ? (<div className="dashboard_wallets">
-        <WalletCard bank="access" data={accessData} />
-        <WalletCard bank="fidelity" data={fidelityData} />
-      </div>
+          </div>
         ) : (
           <div>
             <LoaderSkeleton />
@@ -96,7 +73,10 @@ const DashboardComponent = () => {
           </div>
         )}
       </header>
-      
+      <div className="dashboard_wallets">
+        <WalletCard bank="access" data={accessData} />
+        <WalletCard bank="fidelity" data={fidelityData} />
+      </div>
       <div className="dashboard_stats">
         <StatsCard name="Tickets" amount="30" />
         <StatsCard name="ABSSIN" amount="30" />
@@ -106,4 +86,4 @@ const DashboardComponent = () => {
   );
 };
 
-export default DashboardComponent;
+export default DashbaordComponent;
