ce30389e0c3f67baed6c464be245d9f85560235a
drdonice <doniceubaru1@gmail.com>
2024-06-17 18:50:46 +0100

    Fixed hydration ui issue

--- src/routes/AuthGuard.tsx
+++ src/routes/AuthGuard.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import { useRouter, usePathname } from "next/navigation";
 import { protectedRoutes } from "../routes";
 import { useAuthState } from "../context/authContext";
@@ -10,14 +10,25 @@ const AuthGuard: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   const pathname = usePathname();
 
   const isProtectedRoute = useMemo(() => {
-    return protectedRoutes.some((protectedRoute) => pathname.startsWith(protectedRoute));
+    return protectedRoutes.some((protectedRoute) =>
+      pathname.startsWith(protectedRoute)
+    );
   }, [pathname]);
+  const [isClient, setIsClient] = useState(false);
 
   useEffect(() => {
+    // Set a flag indicating the component has mounted
+    setIsClient(true);
+
     if (!token && isProtectedRoute) {
       router.push("/signin");
     }
   }, [token, isProtectedRoute, router]);
+
+  // Ensure the component matches on both server and client
+  if (!isClient) {
+    return null; // Or a loading indicator
+  }
 
   if (!token && isProtectedRoute) {
     return <Redirecting />;
