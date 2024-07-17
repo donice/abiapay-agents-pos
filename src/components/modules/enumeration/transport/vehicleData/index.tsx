cda39112db6f974d2beb7412645054b7aa0d0482
drdonice <doniceubaru1@gmail.com>
2024-07-17 15:17:58 +0100

    Fixed Api call

--- src/components/modules/enumeration/transport/vehicleData/index.tsx
+++ src/components/modules/enumeration/transport/vehicleData/index.tsx
@@ -2,6 +2,13 @@ import React from "react";
 import { Button } from "@/src/components/common/button";
 import { FormTextInput, SelectInput } from "@/src/components/common/input";
 import { useForm } from "react-hook-form";
+import { useMutation } from "@tanstack/react-query";
+import {
+  verifyPlateNumber,
+  VerifyPlateNumberType,
+} from "@/src/services/transportEnumerationService";
+import { getErrorMessages } from "@/src/utils/helper";
+import toast from "react-hot-toast";
 
 const VehicleData = ({ setStage }: any) => {
   const {
@@ -10,16 +17,33 @@ const VehicleData = ({ setStage }: any) => {
     formState: { errors },
   } = useForm({
     defaultValues: {
-      taxpayer_phone: "",
-      vehicle_plate_number: "",
-      vehicle_category: "",
-      operating_park: "",
-      trade_union: "",
+      merchant_key: process.env.NEXT_PUBLIC_MERCHANT_KEY || "",
+      plate_number: "",
+      phone_number: "",
     },
   });
-  const onSubmit = (data: any) => {
-    console.log(data);
-    setStage(1)
+
+  const { mutate, isPending } = useMutation({
+    mutationFn: (data: VerifyPlateNumberType) => {
+      return verifyPlateNumber(data);
+    },
+    mutationKey: ["verify_plate_number"],
+    onSuccess: (data) => {
+      console.log(data);
+      data.response_code == "00"
+        ? toast.success("Plate Number Verified Successfully")
+        : toast.error("Error Verifying Plate Number");
+    },
+    onError: (error) => {
+      toast.error("Error Verifying Plate Number");
+      console.log(error);
+    },
+  });
+
+  const onSubmit = (reqData: any) => {
+    mutate(reqData);
+    console.log(reqData);
+    // setStage(1);
   };
 
   return (
@@ -28,49 +52,43 @@ const VehicleData = ({ setStage }: any) => {
         <FormTextInput
           label="Driver's Phone Number (e.g 08123456789)"
           type="number"
-          name="taxpayer_phone"
+          name="phone_number"
           placeholder="Enter Driver's Phone Number"
           register={register}
           validation={{ required: true }}
-          error={errors.taxpayer_phone}
+          error={errors.phone_number}
         />
         <FormTextInput
           label="Vehicle Plate Number"
           type="text"
-          name="vehicle_plate_number"
+          name="plate_number"
           placeholder="Enter Vehicle Plate Number"
           register={register}
           validation={{ required: true }}
-          error={errors.vehicle_plate_number}
+          error={errors.plate_number}
         />
         <SelectInput
           label="Vehicle Category"
           name="vehicle_category"
           id="vehicle_category"
-          options={[
-            { label: "Car", value: "Car" },
-          ]}
+          options={[{ label: "Car", value: "Car" }]}
           placeholder="Select Vehicle Category"
         />
         <SelectInput
           label="Operating Park"
           name="operating_park"
           id="operating_park"
-          options={[
-            { label: "Parks", value: "Parks" },
-          ]}
+          options={[{ label: "Parks", value: "Parks" }]}
           placeholder="Select Operating Park"
         />
         <SelectInput
           label="Trade Unions"
           name="trade_union"
           id="trade_union"
-          options={[
-            { label: "Trade Unions", value: "Trade Unions" },
-          ]}
+          options={[{ label: "Trade Unions", value: "Trade Unions" }]}
           placeholder="Select Trade Unions"
         />
-        <Button text="Save & Continue" />
+        <Button text="Save & Continue" loading={isPending} />
       </form>
     </div>
   );
---
+++ src/services/transportEnumerationService.ts
@@ -0,0 +1,26 @@
+import axiosInstance from "../lib/axiosInstance";
+import useIsBrower from "../hooks/useIsBrower";
+import { setToken } from "./setToken";
+
+const url = process.env.NEXT_PUBLIC_BASE_URL;
+
+const isToken =
+  useIsBrower() && window.sessionStorage.getItem("TOKEN")
+    ? window.sessionStorage.getItem("TOKEN")
+    : null;
+setToken(isToken);
+
+export interface VerifyPlateNumberType {
+  phone_number: string,
+  plate_number: string,
+  merchant_key: string
+}
+
+export const verifyPlateNumber = async (requestBody: VerifyPlateNumberType) => {
+  try {
+    const { data } = await axiosInstance.post(`${url}/vehicle/verify-plate-number`, requestBody);
+    return data;
+  } catch (error: any) {
+    throw new Error(`Error fetching transactions: ${error?.message}`);
+  }
+};
