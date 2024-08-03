96e31c99a9e27e13624ef93956ce36025836d276
drdonice <doniceubaru1@gmail.com>
2024-08-03 15:50:01 +0100

    Fixed

--- src/components/modules/tickets/transport/emblem/form/index.tsx
+++ src/components/modules/tickets/transport/emblem/form/index.tsx
@@ -261,8 +261,8 @@ const CreateEmblemForm = ({ setShow }: { setShow: any }) => {
         validation={{ required: true }}
         error={!!errors.wallet_type}
         options={[
-          { label: "Fidelity", value: "Fidelity" },
-          { label: "Access", value: "Access" },
+          { label: "Fidelity", value: "fidelity" },
+          { label: "Access", value: "access" },
         ]}
       />
 
