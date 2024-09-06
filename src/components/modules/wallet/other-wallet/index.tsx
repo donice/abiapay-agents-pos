c43f122ba921148c01ee01a19bc8e485d36e1545
drdonice <doniceubaru1@gmail.com>
2024-09-06 01:34:27 +0100

    onChange

--- src/components/modules/wallet/other-wallet/index.tsx
+++ src/components/modules/wallet/other-wallet/index.tsx
@@ -9,11 +9,14 @@ import { useDebounce } from "@/src/hooks/useDebounce";
 import { fetchWalletInfo } from "@/src/services/walletService";
 import { CustomHeader } from "@/src/components/common/header";
 import toast from "react-hot-toast";
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
 import { SmallLoader } from "@/src/components/common/loader";
+import { fetchDashboardData } from "@/src/services/dashboardService";
 
 const OtherWalletsTransferComponent = () => {
+  const [activeAccount, setActiveAccount] = useState("fidelity");
   const [beneficiary, setBeneficiary] = useState("");
+
   const {
     watch,
     setValue,
@@ -28,9 +31,17 @@ const OtherWalletsTransferComponent = () => {
       desc: "",
     },
   });
+  const { onChange } = register("amount");
 
   const walletNo = watch("recipient_wallet_no");
   const debouncedWalletNo = useDebounce(walletNo, 500);
+
+  const { data } = useQuery({
+    queryKey: ["get_dashboard_data"],
+    queryFn: () => {
+      return fetchDashboardData();
+    },
+  });
 
   const { mutate, isPending } = useMutation({
     mutationFn: (data: string) => {
@@ -41,7 +52,6 @@ const OtherWalletsTransferComponent = () => {
       if (data.status == true) {
         setBeneficiary(data.data.VirtualAccountName);
       } else {
-        // toast.error(data.message);
         setBeneficiary("");
       }
     },
@@ -65,7 +75,11 @@ const OtherWalletsTransferComponent = () => {
   return (
     <section className="other-wallet">
       <CustomHeader title={"Transfer"} desc={"Send money to other wallet"} />
-      <TransferWalletCards />
+      <TransferWalletCards
+        data={data}
+        activeAccount={activeAccount}
+        setActiveAccount={setActiveAccount}
+      />
 
       <form onSubmit={handleSubmit(onSubmit)} className="other-wallet_form">
         <FormTextInput
@@ -77,7 +91,11 @@ const OtherWalletsTransferComponent = () => {
           validation={{ required: true }}
           error={errors.recipient_wallet_no}
         />
-        {isPending && <p className="other-wallet_form_beneficiary"><SmallLoader/> </p>}
+        {isPending && (
+          <p className="other-wallet_form_beneficiary">
+            <SmallLoader />{" "}
+          </p>
+        )}
         {beneficiary !== "" && !isPending && (
           <div className="other-wallet_form_beneficiary">
             <p>{beneficiary}</p>
@@ -90,16 +108,29 @@ const OtherWalletsTransferComponent = () => {
           name={"amount"}
           placeholder="Enter Amount"
           register={register}
-          validation={{ required: true }}
+          onChange={onChange}
+          validation={{
+            required: "Enter an amount",
+            validate: (value: number) => {
+              if (activeAccount == "access") {
+                if (value > data?.access?.wallet_balance) {
+                  return "Insufficient funds in your Access wallet";
+                }
+              } else {
+                if (value > data?.fidelity?.balance) {
+                  return "Insufficient funds in your Fidelity wallet";
+                }
+              }
+            },
+          }}
           error={errors.amount}
         />
+
         <FormTextInput
-          label={"Description"}
+          label={"Description (Optional)"}
           name={"desc"}
           placeholder="Enter Description"
           register={register}
-          validation={{ required: true }}
-          error={errors.desc}
         />
         <Button text="Pay" disabled={beneficiary === ""} />
       </form>
