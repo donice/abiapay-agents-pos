57e5bdd7aa7827e9dc776aa07f6d40977f779cd1
drdonice <doniceubaru1@gmail.com>
2024-09-17 11:41:52 +0100

    Implemented Bank Transfers

--- app/(secured)/wallet/transfer/bank/page.tsx
+++ app/(secured)/wallet/transfer/bank/page.tsx
@@ -1,9 +1,56 @@
-import React from 'react'
+"use client";
+import React from "react";
+import { Button } from "@/src/components/common/button";
+import "./style.scss";
+import { useQuery } from "@tanstack/react-query";
+import { CustomHeader } from "@/src/components/common/header";
+import { fetchAccountStatement } from "@/src/services/accountServices";
+import { Loading } from "@/src/components/common/loader/redirecting";
+import { useRouter } from "next/navigation";
 
-const BankTransferPage = () => {
+const Dynamic = () => {
+  const router = useRouter();
+  const { data, isPending } = useQuery({
+    queryKey: ["bank"],
+    queryFn: fetchAccountStatement,
+  });
+
   return (
-    <div>IN PROGRESS...</div>
-  )
-}
+    <>
+      {" "}
+      <CustomHeader
+        title={"Transfer to other banks"}
+        desc={"Select your preferred bank"}
+      />
+      {isPending ? (
+        <Loading />
+      ) : (
+        <div className="bank">
 
-export default BankTransferPage
+          <div className="bank_cta">
+            {/* <div className="bank_cta_info">
+              <p>
+                Payout requests will be activated when your current earnings are
+                N100, and above
+              </p>
+            </div> */}
+            <Button
+              text={"Fidelity Bank Transfer"}
+              onClick={() => {
+                router.push("/wallet/transfer/bank/fidelity");
+              }}
+            />
+            <Button
+              text={"Access Bank Transfer"}
+              onClick={() => {
+                router.push("/wallet/transfer/bank/access");
+              }}
+            />
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Dynamic;
--- app/(secured)/wallet/transfer/other-wallet/page.tsx
+++ app/(secured)/wallet/transfer/other-wallet/page.tsx
@@ -1,10 +1,56 @@
-import OtherWalletsTransferComponent from '@/src/components/modules/wallet/other-wallet';
-import React from 'react'
+"use client";
+import React from "react";
+import { Button } from "@/src/components/common/button";
+import "./style.scss";
+import { useQuery } from "@tanstack/react-query";
+import { CustomHeader } from "@/src/components/common/header";
+import { fetchAccountStatement } from "@/src/services/accountServices";
+import { Loading } from "@/src/components/common/loader/redirecting";
+import { useRouter } from "next/navigation";
 
-const OtherWalletTransferPage = () => {
+const Dynamic = () => {
+  const router = useRouter();
+  const { data, isPending } = useQuery({
+    queryKey: ["other-wallet"],
+    queryFn: fetchAccountStatement,
+  });
+
   return (
-    <div><OtherWalletsTransferComponent /></div>
-  )
-}
+    <>
+      {" "}
+      <CustomHeader
+        title={"Transfer to other wallets"}
+        desc={"Select wallet"}
+      />
+      {isPending ? (
+        <Loading />
+      ) : (
+        <div className="other-wallet">
 
-export default OtherWalletTransferPage;
+          <div className="other-wallet_cta">
+            {/* <div className="other-wallet_cta_info">
+              <p>
+                Payout requests will be activated when your current earnings are
+                N100, and above
+              </p>
+            </div> */}
+            <Button
+              text={"Fidelity Wallet Transfer"}
+              onClick={() => {
+                router.push("/wallet/transfer/other-wallet/fidelity");
+              }}
+            />
+            <Button
+              text={"Access Wallet Transfer"}
+              onClick={() => {
+                router.push("/wallet/transfer/other-wallet/access");
+              }}
+            />
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Dynamic;
