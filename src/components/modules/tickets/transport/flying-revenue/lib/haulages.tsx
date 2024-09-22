45c551a8a5938f066a6dba7b7685f59aad4556ac
drdonice <doniceubaru1@gmail.com>
2024-09-22 19:03:14 +0100

    IMplemented Flying

--- src/components/modules/tickets/transport/flying-revenue/lib/haulages.tsx
+++ src/components/modules/tickets/transport/flying-revenue/lib/haulages.tsx
@@ -27,7 +27,7 @@ export const haulages: HaulageProps[] = [
   {
     name: "tickets/transport/flying-revenue/produce",
     title: "Produce",
-    cat: "Produce",
+    cat: "Transport",
     icon: <FcShop className="icon" />,
   },
   {
@@ -63,7 +63,7 @@ export const haulages: HaulageProps[] = [
   {
     name: "tickets/transport/flying-revenue/baggage-basket",
     title: "Baggage & Basket Handling Charge",
-    cat: "BaggageBasket",
+    cat: "Baggage_Basket",
     icon: <FcFactory className="icon" />,
   },
   {
