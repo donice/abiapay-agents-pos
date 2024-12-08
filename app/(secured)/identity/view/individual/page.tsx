f9f899b26190035f817985127f75b41a5d22fbf2
drdonice <doniceubaru1@gmail.com>
2024-12-08 17:55:26 +0100

    Implemented View all ABSSIN

--- app/(secured)/identity/view/individual/page.tsx
+++ app/(secured)/identity/view/individual/page.tsx
@@ -1,8 +1,17 @@
+import ViewIndividualAbssinComponent from '@/src/components/modules/identity/view/individual'
+import { Metadata } from 'next/types';
 import React from 'react'
+
+export const metadata: Metadata = {
+  title: "All Individual ABSSINs",
+  description: "Manage all Identities tied to your ABIAPAY account",
+};
 
 const ViewIndividualAbssinPage = () => {
   return (
-    <div>ViewIndividualAbssinPage</div>
+    <div>
+      <ViewIndividualAbssinComponent/>
+    </div>
   )
 }
 
--- src/components/modules/identity/view/individual/index.tsx
+++ src/components/modules/identity/view/individual/index.tsx
@@ -1,9 +1,25 @@
-import React from 'react'
+"use client";
 
-const ViewIndividualAbssinPage = () => {
-  return (
-    <div>ViewIndividualAbssinPage</div>
-  )
-}
+import React from "react";
+import IndividualABSSINStatsCard from "./IndividualABSSINStatsCard";
+import { useQuery } from "@tanstack/react-query";
+import { getIndividualABSSINs } from "@/src/services/identityService";
+import ViewAllIndividualABSSIN from "./ViewAllIndividualABSSIN";
 
-export default ViewIndividualAbssinPage
+const ViewIndividualAbssinComponent = () => {
+  const { data, isLoading } = useQuery({
+    queryKey: ["ticketsWalletData"],
+    queryFn: getIndividualABSSINs,
+  });
+
+  return isLoading ? (
+    <div>Loading...</div>
+  ) : (
+    <div className="grid gap-4">
+      <IndividualABSSINStatsCard data={data} />
+      <ViewAllIndividualABSSIN data={data} />
+    </div>
+  );
+};
+
+export default ViewIndividualAbssinComponent;
