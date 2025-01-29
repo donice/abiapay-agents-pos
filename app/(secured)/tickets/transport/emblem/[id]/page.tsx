e505ba5a421ee10afd6dbc458dc9ad4e080300cd
drdonice <doniceubaru1@gmail.com>
2025-01-29 03:48:44 +0100

    Add Emblem Management feature with view and fetch functionality

--- app/(secured)/tickets/transport/emblem/[id]/page.tsx
+++ app/(secured)/tickets/transport/emblem/[id]/page.tsx
@@ -1,9 +1,12 @@
 "use client";
 import ViewTransportEmblemReceipt from "@/src/components/modules/tickets/transport/emblem/view";
-import { useSearchParams } from "next/navigation";
+import { redirect, useSearchParams } from "next/navigation";
 import React from "react";
 
 const EmblemReceiptPage = ({ params }: { params: { id: string } }) => {
+  if(params.id == "view") {
+    redirect("/tickets/transport/emblem/view");
+  }
   const querySearch = useSearchParams();
   const payment_ref = querySearch.get("payment_ref") || "";
 
--- src/services/ticketsServices.ts
+++ src/services/ticketsServices.ts
@@ -110,6 +110,18 @@ export const fetchTransactions = async () => {
   }
 };
 
+export const fetchAllEmblem = async () => {
+  try {
+    const { data } = await axiosInstance.post(`${url}/transport/fetch-emblem`, {
+      page: 1,
+      limit: 200,
+    });
+    return data;
+  } catch (error: any) {
+    throw new Error(`Error fetching transactions: ${error?.message}`);
+  }
+};
+
 export const retryPayment = async (reqData: { payment_ref: string }) => {
   try {
     const { data } = await axiosInstance.post(
