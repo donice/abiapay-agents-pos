be7119b2900d26f360ed982fb12bdd0ace576ec9
drdonice <doniceubaru1@gmail.com>
2025-03-02 10:52:54 +0100

    feat: enhance file upload component with success dialog and total records display

--- app/(secured)/identity/(types)/infant/bulk-add/page.tsx
+++ app/(secured)/identity/(types)/infant/bulk-add/page.tsx
@@ -1,15 +1,21 @@
 "use client";
-import { Button } from "@/src/components/common/button";
+import { Button, CancelButton } from "@/src/components/common/button";
 import { CustomHeader } from "@/src/components/common/header";
 import { useMutation } from "@tanstack/react-query";
 import { useState } from "react";
-import { TbFileDownload } from "react-icons/tb";
+import {
+  TbCloudUpload,
+  TbFileDownload,
+  TbRosetteDiscountCheckFilled,
+} from "react-icons/tb";
 import { downloadCSV } from "./csv";
 import toast from "react-hot-toast";
+import CustomDialog from "@/src/components/common/modal/CustomDialog";
 
 const FileUpload = () => {
   const [file, setFile] = useState<File | null>(null);
   const [dragActive, setDragActive] = useState(false);
+  const [totalRecords, setTotalRecords] = useState(0);
 
   const handleFileSelect = (event: React.ChangeEvent<HTMLInputElement>) => {
     if (event.target.files && event.target.files.length > 0) {
@@ -65,10 +71,17 @@ const FileUpload = () => {
 
       if (!response.ok) throw new Error("Upload failed");
 
-      const data = await response.json();
-      toast.success(  `✅ Success: ${data.response_message}, Records: ${data.total_records}`
-      );
 
+      const data = await response.json();
+       setTotalRecords(data.total_records);
+       setFile(null);
+
+       toast.success(
+        `✅ Success: ${data.response_message}, Records: ${data.total_records}`
+      );
+      (
+        document.getElementById("createInfantABSSINDialog") as HTMLDialogElement
+      )?.showModal();
     } catch (error) {
       toast.error("❌ Upload failed. Please try again.");
     }
@@ -86,7 +99,7 @@ const FileUpload = () => {
       />
       <div
         onClick={downloadCSV}
-        className="border-2 border-green-400 bg-green-100 border-dashed p-4 rounded-lg flex items-center gap-2 my-0 md:my-4 cursor-pointer"
+        className="border-2 border-green-400 bg-green-100 p-4 rounded-lg flex items-center gap-2 my-0 md:my-4 cursor-pointer"
       >
         <TbFileDownload className="text-5xl text-green-500" />
         <p className="text-xs text-green-600">
@@ -98,9 +111,9 @@ const FileUpload = () => {
         onDragOver={handleDragOver}
         onDragLeave={handleDragLeave}
         onDrop={handleDrop}
-        className={`p-6 border-2 border-dashed rounded-lg text-center cursor-pointer transition ${
-          dragActive ? "border-blue-500 bg-blue-100" : "border-gray-300"
-        }`}
+        className={`p-6 border-2 border-dashed rounded-lg text-center cursor-pointer transition
+          border-blue-500 bg-blue-100
+        `}
       >
         <input
           type="file"
@@ -113,8 +126,11 @@ const FileUpload = () => {
           {dragActive ? (
             <p className="text-blue-600">Drop the file here...</p>
           ) : (
-            <p className="text-gray-600">
-              Drag & drop a CSV file here, or click to select one
+            <p className="text-gray-600 flex flex-col items-center justify-center">
+              <TbCloudUpload className="text-4xl text-blue-500 " />
+              <br />
+              Drag & drop a CSV file here, or click to select one. Only .csv
+              files are allowed.
             </p>
           )}
         </label>
@@ -132,6 +148,38 @@ const FileUpload = () => {
         loading={isPending}
         disabled={isPending || !file}
       />
+
+      <CustomDialog
+        id="createInfantABSSINDialog"
+        onClose={() =>
+          (
+            document.getElementById(
+              "createInfantABSSINDialog"
+            ) as HTMLDialogElement
+          )?.close()
+        }
+      >
+        <div className="flex gap-1 items-center justify-center flex-col text-center">
+          <TbRosetteDiscountCheckFilled className="text-green-600 text-7xl" />
+          <h1 className="text-lg font-semibold">Created Successfully</h1>
+          <p className="text-xs md:text-sm text-gray-400 max-w-[14rem]">
+            You have successfully created {totalRecords} Infant ABSSINs
+          </p>
+          <div className="w-full grid grid-cols-2 gap-2 mt-4">
+            <CancelButton link={"/identity"} />
+            <Button
+              text="Create New"
+              onClick={() => {
+                (
+                  document.getElementById(
+                    "createInfantABSSINDialog"
+                  ) as HTMLDialogElement
+                )?.close();
+              }}
+            />
+          </div>
+        </div>
+      </CustomDialog>
     </div>
   );
 };
