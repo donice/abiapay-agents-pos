0842da3f5c14bf3a5c7aada8503972b0938864d8
shukrah <shuksbakare@gmail.com>
2025-03-19 13:05:58 +0000

    style and display fixes

--- src/components/modules/tickets/receipts/index.tsx
+++ src/components/modules/tickets/receipts/index.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import { CustomHeader } from "@/src/components/common/header";
 import "./style.scss";
 import ReceiptsTable from "./receiptsTable";
+import { GoBackButton } from "@/src/components/common/button";
 
 
 const ReceiptsComponent = () => {
   return (
     <div className="receipts_comp">
+      <GoBackButton />
       <header className="receipts_comp_header">
         <CustomHeader
           title="Receipts"
