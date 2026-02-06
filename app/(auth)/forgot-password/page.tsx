07a1a2815ff3983ec7926af372a634a8d7efd717
John Adetunji <johnadetunji1@gmail.com>
2026-02-06 01:16:03 +0100

    forgot pass commented

--- app/(auth)/forgot-password/page.tsx
+++ app/(auth)/forgot-password/page.tsx
@@ -1,23 +1,23 @@
-import ChangePasswordComponent from "@/src/components/modules/user/settings/change-password";
-import React from "react";
-import "./style.scss";
-import { CustomFormHeader, CustomHeader } from "@/src/components/common/header";
-import type { Metadata } from "next";
+// import ChangePasswordComponent from "@/src/components/modules/user/settings/change-password";
+// import React from "react";
+// import "./style.scss";
+// import { CustomFormHeader, CustomHeader } from "@/src/components/common/header";
+// import type { Metadata } from "next";
 
-export const metadata: Metadata = {
-  title: "Change Password",
-  description: "Reset Password for agent's account",
-};
+// export const metadata: Metadata = {
+//   title: "Change Password",
+//   description: "Reset Password for agent's account",
+// };
 
-const ForgotPasswordPage = () => {
-  return (
-    <div>
-      <CustomFormHeader title="Forgot Password" desc="Reset password for agent's account" />
-      <div className="forgot-password">
-        <ChangePasswordComponent />
-      </div>
-    </div>
-  );
-};
+// const ForgotPasswordPage = () => {
+//   return (
+//     <div>
+//       <CustomFormHeader title="Forgot Password" desc="Reset password for agent's account" />
+//       <div className="forgot-password">
+//         <ChangePasswordComponent />
+//       </div>
+//     </div>
+//   );
+// };
 
-export default ForgotPasswordPage;
+// export default ForgotPasswordPage;
